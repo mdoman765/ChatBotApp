8cbf3a948820a546b4d26f3447e69330d9dd5796
mdoman765 <mdoman765@gmail.com>
2024-12-31 12:46:55 +0600

    Initial commit

---
+++ ChatBotApp/Controllers/ChatBotController.cs
@@ -0,0 +1,14 @@
+﻿using ChatBotApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatBotApp.Controllers
+{
+    public class ChatBotController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new ChatBotModel());
+        }
+    }
+}
+
---
+++ ChatBotApp/Models/ChatBotModel.cs
@@ -0,0 +1,10 @@
+﻿// Models/ChatBotModel.cs
+namespace ChatBotApp.Models
+{
+    public class ChatBotModel
+    {
+        public string UserInput { get; set; }
+        public string BotResponse { get; set; }
+        public string FeatureType { get; set; }
+    }
+}
